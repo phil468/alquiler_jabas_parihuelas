ccb123a51abe70b6849f58629d39c360ea986d0d
phil468 <jdelacruzcalderon@gmail.com>
2025-12-11 08:57:50 -0500

    feat: Revertir la versión de la aplicación a 1.2.0 y el código de versión a 3, desactivar actualización forzada

--- backend/app/Http/Controllers/Api/AppVersionController.php
+++ backend/app/Http/Controllers/Api/AppVersionController.php
@@ -15,10 +15,10 @@ class AppVersionController extends Controller
         return response()->json([
             'success' => true,
             'data' => [
-                'version' => '1.3.0', // Actualiza esto cada vez que publiques una nueva versión
-                'versionCode' => 4, // Incrementa esto en cada release
+                'version' => '1.2.0', // Actualiza esto cada vez que publiques una nueva versión
+                'versionCode' => 3, // Incrementa esto en cada release
                 'downloadUrl' => env('FRONTEND_URL', 'https://apps.vanguardfresh.pe/jabas_y_parihuelas') . '/app-release.apk',
-                'forceUpdate' => true, // Cambia a true si es una actualización crítica
+                'forceUpdate' => false, // Cambia a true si es una actualización crítica
                 //frontend\android\app\build.gradle tambien debe cambiar en versionCode y versionName
                 'releaseNotes' => [
                     'Versión inicial del sistema',
