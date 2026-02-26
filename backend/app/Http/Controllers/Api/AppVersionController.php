5b959e26fb3ce51f50cb96d63e03af59ab675fce
phil468 <jdelacruzcalderon@gmail.com>
2026-02-26 15:21:11 -0500

    feat: Cambiar la opción de actualización forzada a true en la versión actual de la app

--- backend/app/Http/Controllers/Api/AppVersionController.php
+++ backend/app/Http/Controllers/Api/AppVersionController.php
@@ -18,7 +18,7 @@ class AppVersionController extends Controller
                 'version' => '1.2.8', // Actualiza esto cada vez que publiques una nueva versión
                 'versionCode' => 8, // Incrementa esto en cada release
                 'downloadUrl' => env('FRONTEND_URL', 'https://apps.vanguardfresh.pe/jabas_y_parihuelas') . '/app-release.apk',
-                'forceUpdate' => false, // Cambia a true si es una actualización crítica
+                'forceUpdate' => true, // Cambia a true si es una actualización crítica
                 //frontend\android\app\build.gradle tambien debe cambiar en versionCode y versionName
                 'releaseNotes' => [
                     'Mejoras en la estabilidad de la aplicación.',
